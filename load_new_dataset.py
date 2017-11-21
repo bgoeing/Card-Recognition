01b4c831af43a4ceeb7bfc56b2b8465fded05228
matiasct <matiasct@stanford.edu>
2017-11-21 02:34:42 -0800

    uploading new dataset, new vectorsY with 8 classes for both datasets, and new py files for models analysis

---
+++ load_new_dataset.py
@@ -0,0 +1,54 @@
+from PIL import Image, ImageFilter, ImageEnhance
+import io
+import os
+import numpy as np
+import glob
+import cv2
+import random
+from create_images import loadImagesAndNames
+
+
+'''
+I already changed the size and mode of the images to the ones 
+required in our models. So that function is in comments.
+'''
+def changeImageSize():
+
+    realCards, names = loadImagesAndNames('/Users/matiascastillo/Desktop/CS229/Project/newImages/OtherImages/')
+
+    print realCards
+    newImages = []
+    for i, image in enumerate(realCards):
+        im = image.convert('RGB')
+        ima = im.resize((640, 480), Image.ANTIALIAS)
+        newImages.append(ima)
+
+    for i, im in enumerate(newImages):
+        directory = str('/Users/matiascastillo/Desktop/CS229/Project/newImages/resizedCards/'+str(names[i]))
+        im.save(directory, 'JPEG')
+
+
+def loadImagesX(indices, Y_vector):
+    """
+    Indices: indices of the training example to be used
+    Y_vector: The entire Y vector of the training set
+    """
+    filenames = sorted(glob.glob('/Users/matiascastillo/Desktop/CS229/Project/newImages/resizedCards/*.JPG'))
+    print len(filenames)
+    shape = (len(indices),) + np.asarray(cv2.imread(filenames[0])).shape
+    X = np.zeros(shape)
+    Y = np.zeros((len(indices),)+Y_vector[0].shape)
+    for i,index in enumerate(indices):
+        X[i] = cv2.imread(filenames[index])
+        Y[i] = Y_vector[index]
+
+    return X, Y
+
+def main():
+    arr = np.load('vectorY3X.npy')
+    #changeImageSize()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ logistic_2.py
@@ -0,0 +1,35 @@
+from sklearn import linear_model
+import numpy as np
+from load_partial_dataset import loadImages
+import random
+
+def main():
+    random_train = [random.randint(0,8400) for i in range(300)]
+    random_dev = [random.randint(0,8400) for i in range(100)]
+
+    train = list(range(0, 8400, 50))
+    dev = list(range(0, 8400, 49))
+    print train
+    print dev
+
+    arr = np.load('vectorY1.npy')
+    X,Y = loadImages(train, arr)
+    X_flattened = X.reshape((len(X),480*640*3))
+    del X
+    # Convert vectors to value
+    Y_values = [list(y).index(1) for y in Y]
+    del Y
+    model = linear_model.LogisticRegression(solver='newton-cg',multi_class='multinomial')
+    model.fit(X_flattened,Y_values)
+    del X_flattened,Y_values
+
+    X_dev, Y_dev = loadImages(dev, arr)
+    X_dev = X_dev.reshape((len(X_dev),480*640*3))
+    Y_dev = [list(y).index(1) for y in Y_dev]
+    p = model.predict(X_dev)
+    
+    print('Accuracy:')
+    print(model.score(X_dev,Y_dev))
+
+if __name__ == '__main__':
+    main()
