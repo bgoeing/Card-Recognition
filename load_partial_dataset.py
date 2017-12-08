135c63ad7dddeb42eeeda13503cc158958fb6867
Jesper Westell <jesper.westell@gmail.com>
2017-12-07 18:34:11 -0800

    Add minibatch creation functionality

--- load_partial_dataset.py
+++ load_partial_dataset.py
@@ -2,27 +2,68 @@ import numpy as np
 import random
 import glob
 import cv2
+import timeit
 
-randoms = [1630, 777, 3000, 5247, 2519, 1991, 7254, 3333, 6436, 3368, 6672, 737, 479, 701, 3598, 4467, 933, 976, 4393, 7076, 6581, 2716, 972, 5613, 5774, 7164, 715, 845, 4090, 524, 6807, 2863, 1846, 6102, 2644, 2046, 7981, 5927, 1445, 4724, 951, 497, 8399, 8024, 1871, 6287, 6027, 7820, 6868, 2779, 2472, 7997, 6326, 2678, 3075, 2567, 3513, 15, 6333, 1337, 7687, 2327, 8329, 4957, 2776, 6687, 6921, 3300, 6659, 6047, 2583, 1838, 4640, 2777, 1661, 8116, 710, 5454, 3322, 7162, 2515, 2669, 7721, 7609, 6175, 6602, 3860, 2668, 2218, 5193, 6106, 6558, 1837, 1722, 4277, 3381, 3559, 6225, 1825, 5664]
-
-def loadImages(indices, Y_vector):
+def loadImages(indices):
     """
     Indices: indices of the training example to be used
     Y_vector: The entire Y vector of the training set
     """
+    print('loading Y vectors')
+    Y1_vector = np.load('vectorY1.npy')
+    Y2_vector = np.load('vectorY2.npy')
+    print('getting names')
     filenames = sorted(glob.glob('generatedCards/*.JPG'))
     shape = (len(indices),) + np.asarray(cv2.imread(filenames[0])).shape
     X = np.zeros(shape)
-    Y = np.zeros((len(indices),)+Y_vector[0].shape)
+    Y1 = np.zeros((len(indices),) + Y1_vector[0].shape)
+    Y2 = np.zeros((len(indices),) + Y2_vector[0].shape)
+    print('reading images')
     for i,index in enumerate(indices):
         X[i] = cv2.imread(filenames[index])
-        Y[i] = Y_vector[index]
-    return X,Y
+        Y1[i] = Y1_vector[index]
+        Y2[i] = Y2_vector[index]
+    print('done')
+    return X,Y1,Y2
+
+def createBatchIndices(indices,num_batches):
+    """
+    Divides indices into n equally large lists
+    Assumes that indices are already randomized
+    Last batch may be at most num_batches smaller than the other batches
+    """
+    m = len(indices)
+    batches = []
+    batch_size = int(np.ceil(m/num_batches))
+    for i in range(num_batches):
+        batches.append(indices[i*batch_size:(i+1)*batch_size])
+    return batches
 
 def main():
-    arr = np.load('vectorY1.npy')
-    print(loadImages(random,arr))
+    NUM_IMAGES = 8400
+    NUM_BATCHES = 10
+    indices = np.random.permutation(np.arange(NUM_IMAGES))
+    data_dividers = (0.8,0.15,0.05)
+    train_limits = (0,int(data_dividers[0]*NUM_IMAGES))
+    dev_limits = (int(data_dividers[0]*NUM_IMAGES),
+        int(data_dividers[0] * NUM_IMAGES + data_dividers[1]*NUM_IMAGES))
+    test_limits = \
+        (int(data_dividers[0] * NUM_IMAGES + data_dividers[1]*NUM_IMAGES),
+         NUM_IMAGES)
 
+    train_inds = indices[train_limits[0]:train_limits[1]]
+    dev_inds = indices[dev_limits[0]:dev_limits[1]]
+    test_inds = indices[test_limits[0]:test_limits[1]]
+    batches = createBatchIndices(train_inds, NUM_BATCHES)
+
+    # Test: loading all batches, one after another
+    for i in range(len(batches)):
+        print('Batch {0}'.format(i+1))
+        start = timeit.default_timer()
+        X,Y1,Y2 = loadImages(batches[i])
+        end = timeit.default_timer()
+        print('Time taken: {0}'.format(end - start))
+    print('finished')
 
 if __name__ == '__main__':
     main()
